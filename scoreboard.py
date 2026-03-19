a54164c2d6f03ba81f1dc5dbb9fe476d7d71bd40
Rochana Gowda <rochana1906@gmail.com>
2026-03-19 19:39:07 +0530

    snake_game.py
    This project is a classic Snake Game developed using Python’s Turtle module with an Object-Oriented Programming (OOP) approach. 
    The game screen is created using the Turtle graphics window with smooth animation control.
    The Snake class manages the movement and behavior of the snake segments dynamically.
    The Food class generates random food positions that the snake consumes to grow longer.
    The Scoreboard class keeps track of the score and resets it upon collision.
    User input is handled through keyboard controls to move the snake in four directions.
    Collision detection is implemented for walls and the snake’s own body.
    When the snake eats food, it extends and the score increases.
    If the snake hits the boundary or itself, the game resets automatically.
    This project demonstrates core OOP concepts like classes, objects, and modular design in Python.

---
+++ scoreboard.py
@@ -0,0 +1,43 @@
+from turtle import Turtle
+ALIGNMENT= "center"
+FONT=("Arial", 24, "normal")
+
+from unicodedata import normalize
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score=0
+        with open ("data.txt","r") as data:
+            self.high_score=int(data.read())
+        self.color("white")
+        self.penup()
+        self.goto(0,250)
+        self.write(f"score:{self.score}", align=ALIGNMENT, font=FONT)
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"score:{self.score} High score:{self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
+
+        self.score=0
+        self.update_scoreboard()
+
+
+    def increase_score(self):
+            self.score +=1
+            self.clear()
+            self.update_scoreboard()
+
+
+
+
+
+
+
+
